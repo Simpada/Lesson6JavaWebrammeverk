f2bff8010b8b8c6f93289873e1eea482085780b2
Marcus <marcus1sp2@gmail.com>
2022-10-06 17:33:49 +0200

    🟢 library server shouldServeHomePage

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,8 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,7 +12,12 @@ public class LibraryServer {
     private final Server server;
 
     public LibraryServer(int port) {
-        server = new Server(0);
+        server = new Server(port);
+
+        var webContext = new WebAppContext();
+        webContext.setContextPath("/");
+        webContext.setBaseResource(Resource.newClassPathResource("/webappDemo"));
+        server.setHandler(webContext);
     }
 
     public URL getURL() throws MalformedURLException {
